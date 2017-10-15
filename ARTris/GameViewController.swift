c18f211d5fcde4534bf77b2bda7cd426070fb3e1
Alireza Alidousti <alireza.eva.u23@gmail.com>
2017-10-15 02:29:09 -0700

    handles camera orientation

--- ARTris/GameViewController.swift
+++ ARTris/GameViewController.swift
@@ -10,7 +10,45 @@ import UIKit
 import ARKit
 import SceneKit
 
-class GameViewController: UIViewController, ARSCNViewDelegate {
+struct Action: Hashable {
+    let swipe: UISwipeGestureRecognizerDirection
+    let orientation: Int
+    
+    var hashValue: Int {
+        return Int(swipe.rawValue) * 4 + orientation.hashValue
+    }
+    
+    static func == (lhs: Action, rhs: Action) -> Bool {
+        return lhs.swipe == rhs.swipe && lhs.orientation == rhs.orientation
+    }
+    
+    var direction: UISwipeGestureRecognizerDirection { return Action.map[self]! }
+    
+    private static let map: [Action : UISwipeGestureRecognizerDirection] = [
+        // looking forward
+        Action(swipe: .left, orientation: 0) : .left,
+        Action(swipe: .right, orientation: 0) : .right,
+        Action(swipe: .up, orientation: 0) : .up,
+        Action(swipe: .down, orientation: 0) : .down,
+        // looking left
+        Action(swipe: .left, orientation: 1) : .down,
+        Action(swipe: .right, orientation: 1) : .up,
+        Action(swipe: .up, orientation: 1) : .left,
+        Action(swipe: .down, orientation: 1) : .right,
+        // looking back
+        Action(swipe: .left, orientation: 2) : .right,
+        Action(swipe: .right, orientation: 2) : .left,
+        Action(swipe: .up, orientation: 2) : .down,
+        Action(swipe: .down, orientation: 2) : .up,
+        // looking right
+        Action(swipe: .left, orientation: 3) : .up,
+        Action(swipe: .right, orientation: 3) : .down,
+        Action(swipe: .up, orientation: 3) : .right,
+        Action(swipe: .down, orientation: 3) : .left
+    ]
+}
+
+class GameViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     @IBOutlet weak var sceneView: ARSCNView!
     @IBOutlet weak var movementView: UIView! {
         didSet {
@@ -24,9 +62,24 @@ class GameViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
-    var tB = (pos: (0,0,0), 0)
+    private var direction: Double = 0
+    @objc public func session(_ session: ARSession, didUpdate frame: ARFrame){
+        direction = Double(frame.camera.eulerAngles.y)
+    }
+    
+    private var orientation: Int {
+        switch direction {
+        case let d where (-Double.pi / 4 <  d) && (d < Double.pi / 4): return 0
+        case let d where (+Double.pi / 4 <  d) && (d < +3 * Double.pi / 4): return 1
+        case let d where (-Double.pi / 4 >  d) && (d > -3 * Double.pi / 4): return 3
+        default: return 2
+        }
+    }
+    
+    var tB = (pos: (0,0,0), 0)    
     @objc func swipeHandler(byReactingTo swipeRecognizer: UISwipeGestureRecognizer){
-        switch swipeRecognizer.direction {
+        let ori  = Action(swipe: swipeRecognizer.direction, orientation: orientation).direction
+        switch  ori {
         case .right:
             tB = (pos: (tB.pos.0 + 1, tB.pos.1, tB.pos.2), 0)
         case .down:
@@ -50,6 +103,7 @@ class GameViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
+        sceneView.session.delegate = self
         sceneView.scene = SCNScene()
     }
     
