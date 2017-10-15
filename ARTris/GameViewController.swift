54fdcddff7870616f5b26a2ea33c6409ac1ba6a0
Alireza Alidousti <alireza.eva.u23@gmail.com>
2017-10-14 19:40:32 -0700

    adds 3d grid

---
+++ ARTris/GameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ARTris
+//
+//  Created by Alireza Alidousti on 2017-10-04.
+//  Copyright © 2017 ARTris. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import SceneKit
+
+class GameViewController: UIViewController, ARSCNViewDelegate {
+    @IBOutlet weak var sceneView: ARSCNView!
+    let scale: CGFloat = 0.1
+    var grid: Grid!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.scene = SCNScene()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        grid = Grid(w: 10, h: 20, l: 10,
+                    parent: sceneView.scene.rootNode, scale: scale,
+                    color: UIColor.gray.withAlphaComponent(0.7))
+        grid.draw()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+}
+
---
+++ ARTris/Grid.swift
@@ -0,0 +1,73 @@
+//
+//  File.swift
+//  ARTris
+//
+//  Created by Alireza Alidousti on 2017-10-14.
+//  Copyright © 2017 ARTris. All rights reserved.
+//
+
+import SceneKit
+
+class Grid {
+    let w, h, l: Int
+    let parent: SCNNode, scale: CGFloat, color: UIColor
+    let diameter: CGFloat
+    var nodes: [SCNNode] = []
+    
+    init(w: Int, h: Int, l: Int, parent: SCNNode, scale: CGFloat, color: UIColor){
+        self.parent = parent
+        self.scale = scale
+        self.color = color
+        self.w = w; self.h = h; self.l = l
+        self.diameter = scale / 100.0
+    }
+    
+    private func line(pos: (x: CGFloat, y: CGFloat, z: CGFloat),
+                      dim: (w: CGFloat, h: CGFloat, l: CGFloat)) -> SCNNode {
+        let cubeGeometry = SCNBox(width: dim.w, height: dim.h, length: dim.l, chamferRadius: 0.0)
+        let material = SCNMaterial()
+        material.diffuse.contents = color
+        cubeGeometry.materials = [material]
+        let node = SCNNode(geometry: cubeGeometry)
+        node.position = SCNVector3(pos.x + dim.w / 2, pos.y + dim.h / 2, pos.z - dim.l / 2)
+        return node
+    }
+    
+    private func permutation(_ a: Int, _ b: Int) -> [(Int, Int)] {
+        var result: [(Int, Int)] = []
+        for i in 0...a {
+            for j in 0...b {
+                result.append((i, j))
+            }
+        }
+        return result
+    }
+    
+    private func parallelToZAxis() -> [SCNNode] {
+        return permutation(w, h).map { (x, y) in
+            line(pos: (CGFloat(x) * scale, CGFloat(y) * scale, 0 - 3.0),
+                 dim: (diameter, diameter, CGFloat(l) * scale))
+        }
+    }
+    
+    private func parallelToXAxis() -> [SCNNode] {
+        return permutation(l, h).map { (z, y) in
+            line(pos: (0, CGFloat(y) * scale, CGFloat(-z) * scale - 3.0),
+                 dim: (CGFloat(w) * scale, diameter, diameter))
+        }
+    }
+    
+    private func parallelToYAxis() -> [SCNNode] {
+        return permutation(w, l).map { (x, z) in
+            line(pos: (CGFloat(x) * scale, 0, CGFloat(-z) * scale - 3.0),
+                 dim: (diameter, CGFloat(h) * scale, diameter))
+        }
+    }
+    
+    func draw() {
+        for node in parallelToZAxis() { nodes.append(node) } // in -z direction
+        for node in parallelToXAxis() { nodes.append(node) } // in +x direction
+        for node in parallelToYAxis() { nodes.append(node) } // in +y direction
+        for node in nodes { parent.addChildNode(node) }
+    }
+}
